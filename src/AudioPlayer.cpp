a809b743c5673314a8ad1c19aa464f5e5352d545
Eual11 <uchihaeual11@gmail.com>
2024-03-17 00:14:51 +0300

    Playback and Ui implementation

--- src/AudioPlayer.cpp
+++ src/AudioPlayer.cpp
@@ -1,8 +1,9 @@
 #include "../include/AudioPlayer.hpp"
 #include <SDL3/SDL_mixer.h>
+#include <filesystem>
 
-Sonic::SonicAudio::SonicAudio(std::string path) {
-  Mix_Music *tmpMusic = Mix_LoadMUS(path.c_str());
+Sonic::SonicAudio::SonicAudio(std::filesystem::path path) {
+  Mix_Music *tmpMusic = Mix_LoadMUS(path.string().c_str());
   if (!tmpMusic) {
 
     std::cerr << "Couldn't Load Music\n";
@@ -17,8 +18,10 @@ Sonic::SonicAudio::SonicAudio(std::string path) {
     title = Mix_GetMusicTitle(tmpMusic);
     artist = Mix_GetMusicArtistTag(tmpMusic);
     album = Mix_GetMusicAlbumTag(tmpMusic);
-    this->path = path;
+    this->path = path.string();
     duration = Mix_MusicDuration(tmpMusic);
+    if (title.length() == 0)
+      title = path.filename().string();
     if (artist.length() == 0)
       artist = "Unkown Artist";
     if (album.length() == 0)
@@ -60,4 +63,87 @@ Sonic::SonicAudio::SonicAudio(Sonic::SonicAudio &&audio) noexcept {
   this->artist = std::move(audio.artist);
   this->album = std::move(audio.album);
   this->duration = audio.duration;
+}
+
+Sonic::SonicAudioPlayer::SonicAudioPlayer()
+    : VOLUME(100), Current_Music(nullptr), freq(MIX_DEFAULT_FREQUENCY),
+      channels(MIX_DEFAULT_CHANNELS), format(MIX_DEFAULT_FORMAT) {
+
+  std::cout << "AudioPlayer starting with default settings\n";
+
+  SDL_AudioSpec spec;
+  spec.freq = freq;
+  spec.channels = channels;
+  spec.format = format;
+  /* if(Mix_OpenAudio(0 ) */
+
+  if (Mix_OpenAudio(0, &spec) < 0) {
+    // do something
+    //
+    std::cerr << "Couldn't Open Audio device\n";
+    std::cerr << Mix_GetError() << std::endl;
+    return;
+  }
+}
+Sonic::SonicAudioPlayer::SonicAudioPlayer(int _frequency, int _channels,
+                                          SDL_AudioFormat _format)
+    : VOLUME(100), Current_Music(nullptr), freq(_frequency),
+      channels(_channels), format(_format) {
+
+  SDL_AudioSpec spec;
+  spec.freq = freq;
+  spec.channels = channels;
+  spec.format = format;
+
+  if (Mix_OpenAudio(0, &spec) < 0) {
+    // do something
+    //
+    std::cerr << "Couldn't Open Audio device\n";
+    std::cerr << Mix_GetError() << std::endl;
+    exit(1);
+  }
+
+  // reseting the specifications based on the specs given by the audio device
+  Mix_QuerySpec(&freq, &format, &channels);
+  std::cerr << "Frequency: " << freq << "\n";
+  std::cerr << "Format: " << (int)format << "\n";
+  std::cerr << "Channels: " << channels << "\n";
+}
+
+Sonic::SonicAudioPlayer::~SonicAudioPlayer() {
+  if (Current_Music)
+    Mix_FreeMusic(Current_Music);
+}
+bool Sonic::SonicAudioPlayer::isSupported(std::string extention) {
+  return (std::find(m_SupportedFormats.begin(), m_SupportedFormats.end(),
+                    extention) != m_SupportedFormats.end());
+}
+
+void Sonic::SonicAudioPlayer::TogglePlay(const SonicAudio &audio) {
+  if (Mix_PlayingMusic()) {
+    // pause play back if it is the same song
+    if (Current_SonicAudio.path == audio.path) {
+      if (Mix_PausedMusic())
+        Mix_ResumeMusic();
+      else
+        Mix_PauseMusic();
+    } else {
+
+      // TODO: maybe cross fading and other effects
+      Mix_HaltMusic(); // brute force stopping
+      Mix_FreeMusic(Current_Music);
+      Current_Music = nullptr;
+      Current_Music = Mix_LoadMUS(audio.path.c_str());
+
+      Mix_PlayMusic(Current_Music, NUM_LOOPS);
+    }
+  } else {
+    if (Current_Music)
+      Mix_FreeMusic(Current_Music);
+    Current_Music = nullptr;
+    Current_Music = Mix_LoadMUS(audio.path.c_str());
+
+    Mix_PlayMusic(Current_Music, NUM_LOOPS);
+  }
+  Current_SonicAudio = audio;
 }
--- src/SonicUI.cpp
+++ src/SonicUI.cpp
@@ -1,21 +1,68 @@
 #include "../include/SonicUI.hpp"
+#include <SDL3/SDL_mixer.h>
+#include <SDL3/SDL_system.h>
+#include <algorithm>
+#include <filesystem>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/component_base.hpp>
 #include <ftxui/component/event.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/dom/node.hpp>
 
-Sonic::SonicUI::SonicUI() {
+namespace fs = std::filesystem;
 
+Sonic::SonicUI::SonicUI(Sonic::SonicUIOptions options) {
+
+  m_TrackSelector = TrackSelection::ARTIST;
   ftxui::Component left_main =
       ftxui::Container::Horizontal({LeftPanel, MainWindow});
   ftxui::Component sonicui =
       ftxui::Container::Vertical({left_main, StatusLine});
 
+  fs::path audio_dir(options.new_path);
+  if (!fs::exists(audio_dir) || !fs::is_directory(audio_dir)) {
+    std::cout << "Path: " << audio_dir
+              << "doesn't exist or it's not a valid dir\n";
+    exit(1);
+  }
+
+  SonicAudioPlayer AudioPlayer(options.freq, options.channels, options.format);
+  Load_Libraries(audio_dir);
   Add(sonicui);
 }
 
 bool Sonic::SonicUI::OnEvent(ftxui::Event event) {
+
+  // p: play/pause
+  // n: next track
+  // b: back track
+  // left/right arrow: perhaps sicking ?
+  // q: quit
+  if (LeftPanel->Focused() && event == ftxui::Event::ArrowDown) {
+    UpdateAudioQueue((m_LeftPanelSelected + 1) % LeftPanelSelection.size());
+  } else if (LeftPanel->Focused() && event == ftxui::Event::ArrowUp) {
+    UpdateAudioQueue((m_LeftPanelSelected - 1));
+  }
+  if (event == ftxui::Event::ArrowLeft) {
+    return true;
+  }
+  if (event == ftxui::Event::ArrowRight) {
+    return true;
+  }
+  if (event == ftxui::Event::Tab) {
+    if (LeftPanel->Focused()) {
+
+      MainWindow->TakeFocus();
+    } else if (MainWindow->Focused()) {
+      LeftPanel->TakeFocus();
+    }
+    return true;
+  }
+  if (event == ftxui::Event::Character('p')) {
+    if (MainWindow->Focused())
+      AudioPlayer.TogglePlay(AudioQueue[m_MainWindowSelected]);
+    return true;
+  }
 
   return ftxui::ComponentBase::OnEvent(event);
 }
@@ -23,7 +70,7 @@ bool Sonic::SonicUI::OnEvent(ftxui::Event event) {
 ftxui::Element Sonic::SonicUI::Render() {
 
   ftxui::Element mainView = ftxui::hbox(
-      {LeftPanel->Render() | ftxui::borderLight | ftxui::yframe |
+      {LeftPanel->Render() | ftxui::frame | ftxui::borderLight |
            ftxui::size(ftxui::WIDTH, ftxui::EQUAL, 30),
        MainWindow->Render() | ftxui::yframe | ftxui::vscroll_indicator |
            ftxui::flex | ftxui::borderLight});
@@ -34,4 +81,108 @@ ftxui::Element Sonic::SonicUI::Render() {
       ftxui::size(ftxui::HEIGHT, ftxui::EQUAL, 5);
 
   return ftxui::vbox({mainView | ftxui::flex, statusline_view});
+}
+
+void Sonic::SonicUI::Load_Libraries(std::filesystem::path new_path) {
+
+  // load audio trachs from libraries
+
+  for (auto dir_file : std::filesystem::directory_iterator(new_path)) {
+    if (dir_file.is_regular_file() &&
+        AudioPlayer.isSupported(dir_file.path().extension().string())) {
+
+      Sonic::SonicAudio audio(fs::absolute(dir_file.path()));
+      // if the audio file is a valid playable music file
+
+      // TODO: needs optimization
+      if (audio.path != "") {
+        TracksList.push_back(audio);
+      }
+    }
+  }
+  UpdateAllSelection();
+}
+void Sonic::SonicUI::UpdataLeftPanelSelection(void) {
+  LeftPanelSelection.clear();
+  for (auto track : TracksList) {
+    switch (m_TrackSelector) {
+    case TrackSelection::ARTIST: {
+      if (std::find(LeftPanelSelection.begin(), LeftPanelSelection.end(),
+                    track.artist) == LeftPanelSelection.end())
+        LeftPanelSelection.push_back(track.artist);
+      std::cerr << track.artist << std::endl;
+
+      break;
+    }
+    case TrackSelection::ALBUM: {
+      /* std::cerr << "Supported ALBUM\n"; */
+      if (std::find(LeftPanelSelection.begin(), LeftPanelSelection.end(),
+                    track.album) == LeftPanelSelection.end())
+        LeftPanelSelection.push_back(track.album);
+
+      //        LeftPanelSelection.push_back(track.album);
+      break;
+    }
+    case TrackSelection::PLAYLIST: {
+      // unimplmented
+      break;
+    }
+    }
+  }
+  UpdateLeftPanelView();
+  // update the left panel view and mainwindow view
+}
+
+void Sonic::SonicUI::UpdateAudioQueue(int index) {
+  index = index < 0 ? 0 : index;
+  AudioQueue.clear();
+  for (auto track : TracksList) {
+    switch (m_TrackSelector) {
+    case TrackSelection::ALBUM: {
+      if (track.album == LeftPanelSelection[index]) {
+        AudioQueue.push_back(track);
+      }
+
+      break;
+    }
+    case TrackSelection::ARTIST: {
+      if (track.artist == LeftPanelSelection[index]) {
+        AudioQueue.push_back(track);
+        // do something
+      }
+      break;
+    }
+    case TrackSelection::PLAYLIST: {
+      // unimplmented
+      break;
+    }
+    }
+  }
+
+  UpdateMainWindowView();
+}
+void Sonic::SonicUI::UpdateLeftPanelView(void) {
+
+  LeftPanel->DetachAllChildren();
+
+  for (auto &selection : LeftPanelSelection) {
+    LeftPanel->Add(ftxui::MenuEntry(selection, LeftPanelEntryOption));
+  }
+}
+
+void Sonic::SonicUI::UpdateMainWindowView(void) {
+
+  MainWindow->DetachAllChildren();
+
+  std::cerr << "AudioQueue size: " << AudioQueue.size() << std::endl;
+  for (auto &track : AudioQueue) {
+    /* std::cerr << "title: " << track.title << " artist: " << track.artist */
+    /* << " album; " << track.album << " path: " << track.path */
+    /* << std::endl; */
+    MainWindow->Add(ftxui::MenuEntry(track.title, MainWindowEntryOption));
+  }
+}
+void Sonic::SonicUI::UpdateAllSelection(void) {
+  UpdataLeftPanelSelection();
+  UpdateAudioQueue(m_LeftPanelSelected);
 }
