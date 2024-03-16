0752191df2ab57e1313f8d13d478966a81e6502a
Eual11 <uchihaeual11@gmail.com>
2024-03-17 00:13:31 +0300

    Implemented audio playback, pause and resume, changed the parameter for audio path from string to std::filesystem path

--- include/AudioPlayer.hpp
+++ include/AudioPlayer.hpp
@@ -2,6 +2,8 @@
 #define _AUDIOPLAYER_HPP
 #include <SDL3/SDL_audio.h>
 #include <SDL3/SDL_mixer.h>
+#include <algorithm>
+#include <filesystem>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -21,12 +23,11 @@ namespace Sonic {
 class SonicAudio {
 public:
   SonicAudio(); // default constructor
-  SonicAudio(std::string path);
+  SonicAudio(std::filesystem::path path);
   SonicAudio(const SonicAudio &audio);            // copy constructor
   SonicAudio &operator=(const SonicAudio &audio); // assignment operator
   SonicAudio(SonicAudio &&audio) noexcept;        // move constructor
 
-private:
   std::string title;  // audio title
   std::string artist; // artist
   std::string album;
@@ -39,14 +40,14 @@ class SonicAudioPlayer {
 
 public:
   SonicAudioPlayer();
-  SonicAudioPlayer(SonicAudio);
   SonicAudioPlayer(int, int, SDL_AudioFormat);
-  SonicAudioPlayer(Mix_Music *);
+  /* SonicAudioPlayer(Mix_Music *); */
   ~SonicAudioPlayer(void);
   // getter and setter methods for player audio
   int getVolume(void);
   void setVolume(int vol);
 
+  bool isSupported(std::string);
   // duration and seeking operations
   double getCurrentAudioDuration();
   void seekCurrentAudio(double);
@@ -55,18 +56,22 @@ public:
 
   // repeating
   void toggleRepeat();
-  void Play();
+  void TogglePlay(const Sonic::SonicAudio &);
   void rewindCurrentAudio();
   void pauseCurrentAudio();
   void ResumeCurrentAudio();
 
 private:
   int VOLUME;
-  Mix_Music *Current_Audio;
+  Mix_Music *Current_Music;
+  SonicAudio Current_SonicAudio;
+  int NUM_LOOPS = 0;
   int freq;
   int sample_size;
   int channels;
   SDL_AudioFormat format;
+  std::vector<std::string> m_SupportedFormats = {".mp3", ".wav", ".ogg",
+                                                 ".flac"};
 };
 }; // namespace Sonic
 #endif
