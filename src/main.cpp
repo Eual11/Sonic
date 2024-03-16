505d1ca03a5f716a07235eb9ee01e84e6dffb70f
Eual11 <uchihaeual11@gmail.com>
2024-03-17 00:14:36 +0300

    adding options to SonicUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include "../include/SonicUI.hpp"
+#include <SDL3/SDL_mixer.h>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <iostream>
 int main(int argc, char **argv) {
 
+  if (argc < 2) {
+    std::cout << "Usage: " << argv[0] << " [Audio_Dir]\n";
+    return -1;
+  }
+
+  Sonic::SonicUIOptions options;
+  options.freq = MIX_DEFAULT_FREQUENCY;
+  options.channels = MIX_DEFAULT_CHANNELS;
+  options.format = MIX_DEFAULT_FORMAT;
+  options.new_path = argv[1];
   auto scr = ftxui::ScreenInteractive::Fullscreen();
-  auto sonic = ftxui::Make<Sonic::SonicUI>();
+  auto sonic = ftxui::Make<Sonic::SonicUI>(options);
 
   scr.Loop(sonic);
 
