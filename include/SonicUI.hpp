7306b639aa0cfdc1c83727f83ee46625667fa5c6
Eual11 <uchihaeual11@gmail.com>
2024-03-17 00:14:03 +0300

    Overhauling the ui and adding basic playback, resume and pause functionality

--- include/SonicUI.hpp
+++ include/SonicUI.hpp
@@ -1,35 +1,62 @@
 #ifndef _SONICUI_HPP
 #define _SONICUI_HPP
 #include "../include/AudioPlayer.hpp"
+#include <filesystem>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/component_base.hpp>
+#include <ftxui/component/component_options.hpp>
 #include <ftxui/component/event.hpp>
 #include <ftxui/dom/node.hpp>
 #include <set>
 #include <string>
 
-namespace Sonic {
+enum class TrackSelection {
 
+  ARTIST,
+  ALBUM,
+  PLAYLIST
+};
+
+namespace Sonic {
+struct SonicUIOptions {
+
+  int freq;
+  int channels;
+  SDL_AudioFormat format;
+  std::string new_path;
+};
 class SonicUI : public ftxui::ComponentBase {
 
 private:
-  int m_LeftPanelSelected;
-  int m_MainWindowSelected;
+  int m_LeftPanelSelected = 0;
+  int m_MainWindowSelected = 0;
 
+  TrackSelection m_TrackSelector;
   ftxui::Component LeftPanel =
       ftxui::Container::Vertical({}, &m_LeftPanelSelected);
   ftxui::Component MainWindow =
       ftxui::Container::Vertical({}, &m_MainWindowSelected);
 
   ftxui::Component StatusLine = ftxui::Container::Horizontal({});
-  std::set<std::string> LeftPanelSelection;
-  std::vector<Sonic::SonicAudio> AudioSelection;
+  std::vector<std::string> LeftPanelSelection;
+  std::vector<Sonic::SonicAudio> TracksList;
+  std::vector<Sonic::SonicAudio> AudioQueue;
+  void Load_Libraries(std::filesystem::path);
+  void UpdataLeftPanelSelection(void);
+  void UpdateAudioQueue(int index);
+  void UpdateLeftPanelView(void);
+  void UpdateAllSelection(void);
+  void UpdateMainWindowView(void);
+  ftxui::MenuEntryOption LeftPanelEntryOption;
+  ftxui::MenuEntryOption MainWindowEntryOption;
+  SonicAudioPlayer AudioPlayer;
 
 public:
-  SonicUI(); // constructor might need options
+  SonicUI(SonicUIOptions); // constructor might need options
   bool OnEvent(ftxui::Event) override;
   ftxui::Element Render() override;
 };
+
 } // namespace Sonic
 
 #endif
