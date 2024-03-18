5b86b3a4ec53bb980db4829013156fccf9195506
Eual11 <uchihaeual11@gmail.com>
2024-03-18 12:38:33 +0300

    bug fixes and features

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "../include/SonicUI.hpp"
 #include <SDL3/SDL_mixer.h>
+#include <chrono>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <iostream>
@@ -17,8 +18,23 @@ int main(int argc, char **argv) {
   options.new_path = argv[1];
   auto scr = ftxui::ScreenInteractive::Fullscreen();
   auto sonic = ftxui::Make<Sonic::SonicUI>(options);
+  std::atomic<bool> refresh_ui_continue(true);
+  std::thread refresh_ui([&] {
+    while (refresh_ui_continue) {
+
+      using namespace std::chrono_literals;
+      std::this_thread::sleep_for(0.75s); // make this thread sleep for 0.75
+                                          // secs
+
+      // request a new frame to be rendered every 0.75 secs, this si
+      // approimately 2 frames persecond
+      scr.PostEvent(ftxui::Event::Custom);
+    }
+  });
 
   scr.Loop(sonic);
 
+  refresh_ui_continue = false;
+  refresh_ui.join();
   return 0;
 }
