dc1a91c35e26cacf4a9036389eadfeedc33a8b18
Eual11 <uchihaeual11@gmail.com>
2024-03-18 12:38:01 +0300

    utility header

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,4 +1,8 @@
 #ifndef _SONIC_UTILS
 #define _SONIC_UTILS
-
+#include <algorithm>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/color.hpp>
+#include <string>
+namespace Sonic {} // namespace Sonic
 #endif
